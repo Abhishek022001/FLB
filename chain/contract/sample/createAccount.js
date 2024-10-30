bf5443bb15a89113226fec1bafcea4c48a515c72
Ablus-Bang <Albus@q-stars.io>
2024-10-30 19:53:37 +1100

    New FLB

--- chain/contract/sample/createAccount.js
+++
@@ -1,9 +0,0 @@
-import {ethers} from "ethers";
-
-export const createAccount = () => {
-    const userAccount = ethers.Wallet.createRandom();
-    console.log("user account privateKey:", userAccount.privateKey);
-    console.log("user account address:", userAccount.address);
-}
-
-createAccount()
--- run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-python run_grpc_server.py &
-python run_fast_api.py &
-wait
